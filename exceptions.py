4a921c8e0dc931e69b7bc3a28a80fb195d28c9c9
Rombel1 <rombel1011@gmail.com>
2024-12-01 22:28:46 +0300

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,14 @@
+class InvalidVehicleTypeException(
+    Exception
+):  # Исключение возникающее при выборе неверного типа транспорта
+    def __init__(self, vehicle_type):
+        super().__init__(f"Неверный тип транспорта: {vehicle_type}. ")
+
+
+class InvalidFileTypeException(
+    Exception
+):  # Исключение возникающее при выборе неверного типа файла
+    def __init__(self, file_type):
+        super().__init__(
+            f"Неверный тип файла: {file_type}. Допустимые типы: json, xml."
+        )
